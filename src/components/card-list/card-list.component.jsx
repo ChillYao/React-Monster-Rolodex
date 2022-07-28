0e4c6c2f661f7aec3e289a3e8fcec1a684f20d0e
“ChillYao” <51960333+ChillYao@users.noreply.github.com>
2022-07-28 15:25:41 +0200

    refactor card and cardlist

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,19 +1,16 @@
 import React from 'react';
-import { Component } from 'react';
 import Card from '../card/card.component';
 import './card-list.component.css';
 
-class CardList extends Component {
-    render() {
-        const { filteredMonsters } = this.props;
-        return (
-            <div className="card-list">
-                {filteredMonsters.map((monster) => {
-                    return <Card monster={monster} />;
-                })}
-            </div>
-        );
-    }
-}
+const CardList = (props) => {
+    const { filteredMonsters } = props;
+    return (
+        <div className="card-list">
+            {filteredMonsters.map((monster) => {
+                return <Card monster={monster} />;
+            })}
+        </div>
+    );
+};
 
 export default CardList;
